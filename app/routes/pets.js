d1b9b44f6ab9a78e1aca5b91f4eb4c70fcc88025
hammer200911 <jdaniel231@gmail.com>
2021-12-07 16:22:14 -0300

    update pet

--- app/routes/pets.js
+++ app/routes/pets.js
@@ -29,6 +29,25 @@ router.get('/:id', withAuth, async(req, res) =>{
   }
 })
 
+router.put('/:id', withAuth, async(req, res) => {
+  const {  name, nameAnimal, animal_type, age, weight } = req.body;
+  const { id } = req.params;
+  
+  try {
+    let pet = await Pet.findById(id);
+    if(isOwner(req.user, pet)){
+      pet = await Pet.findByIdAndUpdate(id,
+        { $set: {name: name, nameAnimal: nameAnimal, animal_type: animal_type, age: age, weight: weight} },
+        { upsert: true, 'new': true }  
+      );
+      res.json(pet)
+    } else
+      res.status(403).json({error: 'Permissão negado'});
+  } catch (error) {
+    res.status(500).json({error: 'Problema para atualizar a pet'});
+  }
+})
+
 router.get('/', withAuth, async (req, res) => {
   try {
     let pets = await Pet.find({author: req.user._id })
